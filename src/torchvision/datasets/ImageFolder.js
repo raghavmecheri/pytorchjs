50a130730ff372aa15697b8e22f74df2fce603e3
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-10-01 11:09:00 +0530

    Removed extensions in order to conform with torchvision definition

--- src/torchvision/datasets/ImageFolder.js
+++ src/torchvision/datasets/ImageFolder.js
@@ -10,7 +10,7 @@ const defaultLoader = (imagePath) => {
 }
 
 export default class ImageFolder extends DatasetFolder {
-    constructor(root, loader=defaultLoader, extensions=null, transform=null, targetTransform=null, isValidFile=null) {
+    constructor(root, loader=defaultLoader, transform=null, targetTransform=null, isValidFile=null) {
         super(root, loader, (isValidFile === null ? IMAGE_EXTENSIONS : null), transform, targetTransform, isValidFile);
         this.imgs = this.samples;
     }

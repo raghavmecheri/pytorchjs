4deeb6357e8a75c0ccecd2d90a703389100ad5f1
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-10-04 23:36:37 +0530

    Tested multiple batchSize values

--- test/torch/Dataloader.test.js
+++ test/torch/Dataloader.test.js
@@ -15,8 +15,35 @@ describe("DataLoader expected functionality", () => {
     let items = 0;
     while (dataIterator.hasNext()) {
       const next = dataIterator.nextData();
+      expect(next.length).toEqual(1);
       expect(next).not.toEqual(null);
       items += 1;
+    }
+    expect(items).toEqual(6);
+  });
+
+  test("Load data via DataLoader iterator given batchSize=2", () => {
+    const loader = new DataLoader(dataset, 2);
+    const dataIterator = loader.getIterator();
+    let items = 0;
+    while (dataIterator.hasNext()) {
+      const next = dataIterator.nextData();
+      expect(next.length).toEqual(2);
+      expect(next).not.toEqual(null);
+      items += 2;
+    }
+    expect(items).toEqual(6);
+  });
+
+  test("Load data via DataLoader iterator given batchSize=3", () => {
+    const loader = new DataLoader(dataset, 3);
+    const dataIterator = loader.getIterator();
+    let items = 0;
+    while (dataIterator.hasNext()) {
+      const next = dataIterator.nextData();
+      expect(next.length).toEqual(3);
+      expect(next).not.toEqual(null);
+      items += 3;
     }
     expect(items).toEqual(6);
   });

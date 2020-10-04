e4a12c73d6b9eeb91b6cae12ca7c552fcd3da56e
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-10-04 22:13:24 +0530

    MVP dataloader framework

--- src/torch/utils/data/DataLoader.js
+++ src/torch/utils/data/DataLoader.js
@@ -1,7 +1,42 @@
+class _BaseDataLoaderIter {
+  constructor(dataloader) {
+    this.dataset = dataloader.dataset;
+    this.batchSize = dataloader.batchSize;
+    this.samples = dataloader.dataset.samples;
+  }
+}
+
+class _SingleProcessDataloaderIter extends _BaseDataLoaderIter {
+  constructor(dataloader) {
+    super(dataloader);
+    this.index = 0;
+  }
+
+  nextData = () => {
+    const { startIndex, endIndex } = this._nextIndex();
+    const data = this._fetch(startIndex, endIndex);
+    return data;
+  };
+
+  _fetch = (startIndex, endIndex) => {
+    const targets = this.samples.slice(startIndex, endIndex);
+    return targets.map((target) => this.dataset.getItem(target));
+  };
+
+  _nextIndex = () => {
+    const { index, batchSize } = this;
+    this.index += batchSize;
+    return { startIndex: index, endIndex: this.index };
+  };
+}
+
 export default class DataLoader {
-  constructor(dataset, batchSize = 1, shuffle = false) {
+  constructor(dataset, batchSize = 1) {
     this.dataset = dataset;
     this.batchSize = batchSize;
-    this.shuffle = shuffle;
   }
+
+  getIterator = () => {
+    return _SingleProcessDataloaderIter(this);
+  };
 }

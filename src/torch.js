c7777f718bd14d46ee8d53cb4c98c7d79a34dbc6
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-10-10 13:04:54 +0530

    Added @arition/torchjs as a dependancy + some skeleton code

--- src/torch.js
+++ src/torch.js
@@ -1,8 +1,10 @@
 // import torch from "./torch/torch";
 // import nn from "./torch/nn";
 // import cuda from "./torch/cuda";
-import utils from './torch/utils/utils';
+import utils from "./torch/utils/utils";
+import load from "./torch/load/index";
 
 module.exports = {
   utils,
+  load,
 };

56ccbb55fe3ed041972a04c43f361829d46ee42d
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-10-20 08:14:57 +0530

    Added default test + added linter call to travis

--- test/torchvision/transforms/transform.test.js
+++ test/torchvision/transforms/transform.test.js
@@ -2,47 +2,69 @@ import { describe, expect, test } from "@jest/globals";
 import fs from "fs";
 import { torchvision } from "../../../src/index";
 
-const { InvertAxes, Resize, Grayscale, Compose } = torchvision.transforms;
+const {
+  InvertAxes,
+  Resize,
+  Grayscale,
+  Compose,
+  DefaultTransform,
+} = torchvision.transforms;
 
 const nj = require("numjs");
 
 const fetchTestImage = () => {
   const buffer = fs.readFileSync("./test/resources/meme.jpg");
-  return nj.images.read(buffer);
+  return {
+    image: nj.images.read(buffer),
+    height: 224,
+    width: 524,
+    channels: 3,
+  };
 };
 
 describe("InvertAxes transform test", () => {
   test("Testing InvertAxes call given valid numjs object", () => {
-    const image = fetchTestImage();
-    expect(new InvertAxes()(image).shape).toEqual([3, 224, 524]);
+    const { image, height, width, channels } = fetchTestImage();
+    expect(new InvertAxes()(image).shape).toEqual([channels, height, width]);
   });
 });
 
 describe("Resize transform test", () => {
   test("Testing Resize call given valid numjs object", () => {
-    const image = fetchTestImage();
+    const { image, channels } = fetchTestImage();
     expect(new Resize({ height: 224, width: 224 })(image).shape).toEqual([
       224,
       224,
-      3,
+      channels,
     ]);
   });
 });
 
 describe("Grayscale transform test", () => {
   test("Testing Grayscale call given valid numjs object", () => {
-    const image = fetchTestImage();
-    expect(new Grayscale()(image).shape).toEqual([224, 524]);
+    const { image, height, width } = fetchTestImage();
+    expect(new Grayscale()(image).shape).toEqual([height, width]);
+  });
+});
+
+describe("DefaultTransform transform test", () => {
+  test("Testing default call given valid numjs object", () => {
+    const { image, height, width, channels } = fetchTestImage();
+    expect(new DefaultTransform()(image).shape).toEqual([
+      height,
+      width,
+      channels,
+    ]);
   });
 });
 
 describe("Compose transform test", () => {
   test("Testing composed Transform given valid numjs object", () => {
-    const image = fetchTestImage();
+    const { image, channels } = fetchTestImage();
     const compose = new Compose([
       new Resize({ height: 224, width: 224 }),
       new InvertAxes(),
     ]);
-    expect(compose(image).shape).toEqual([3, 224, 224]);
+    expect(compose(image).shape).toEqual([channels, 224, 224]);
   });
 });
